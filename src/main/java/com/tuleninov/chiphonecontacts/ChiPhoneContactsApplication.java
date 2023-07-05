c914f26310fa1a652e9f38eb49da0a62dfdccec9
Alexander Tuleninov <talextu@gmail.com>
2023-07-05 06:58:07 +0300

    init commit

---
+++ src/main/java/com/tuleninov/chiphonecontacts/ChiPhoneContactsApplication.java
@@ -0,0 +1,13 @@
+package com.tuleninov.chiphonecontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiPhoneContactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiPhoneContactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuleninov/chiphonecontacts/ChiPhoneContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuleninov.chiphonecontacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiPhoneContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
